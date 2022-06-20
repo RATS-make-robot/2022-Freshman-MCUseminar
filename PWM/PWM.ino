775e656e99ec3069ea508832c27c57594bb5a6f9
Jongsang yoo <jamin0513@gmail.com>
2022-06-21 00:37:49 +0900

    pwm memo

--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -1,6 +1,11 @@
 void setup(){ //328 레지스터로 변경
     DDRB = 0xFF;  // OC0(PB4)출력설정-> 변경필요
-    TCCR0 = 0b01111010;    // fast PWM mode : 3,6(WGM01,00) - 11, 5,4(COM ) - 11, 2-0(CS) - 010
+
+    TCCR0A |= (1<<WGM01) | (0<<WGM00);
+    TCCR0B |= (1<<WGM02) | (0<<WGM03);   //16진수 떄려박기로 변경
+    TCCR0B |= (1<<CS022);
+    TCCR0 = 0b01111010;    // fast PWM mode : 3,6(WGM01,00)
+                            // - 11, 5,4(COM ) - 11, 2-0(CS) - 010
 }
 
 void loop(){
@@ -15,3 +20,6 @@ void loop(){
     OCR0 = 0xFF;    //255-> 255/255 = 100%
     delay_ms(500);
 }
+
+// https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=schmacher&logNo=221405989919 참고
+// https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=schmacher&logNo=221405989919
